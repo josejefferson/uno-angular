10f1e317a9f97a122fd119f1d0d77abdd3429e2a
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-06-11 19:28:11 +0000

    ???

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RoomsComponent } from './pages/rooms/rooms.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, RoomsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
