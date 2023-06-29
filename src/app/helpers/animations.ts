d094846a2c7dab2b3fcec953ebf591247f62ddfb
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-06-29 19:55:56 +0000

    fix: Corrigido bug na animação das cartas

--- src/app/helpers/animations.ts
+++ src/app/helpers/animations.ts
@@ -8,7 +8,7 @@ export const zoomAnimation = trigger('zoom', [
   ]),
   transition(':leave', [
     style({ opacity: 1, transform: 'scale(1)', width: '*' }),
-    animate(200, style({ opacity: 0, transform: 'scale(10)' })),
+    animate(200, style({ opacity: 0, transform: 'scale(0)' })),
     animate(100, style({ width: 0 }))
   ])
 ])
